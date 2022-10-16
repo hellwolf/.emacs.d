e067203e3307987b0952170bcd316f64d1758e03
Miao, ZhiCheng <miao@superfluid.finance>
2022-10-16 21:02:25 +0300

    make emacs-project.el possible name too

--- my.features/91-emacs-project.el
+++ my.features/91-emacs-project.el
@@ -11,7 +11,8 @@
   (interactive "DDirectory of a .emacs-project: ")
   (let*
     ((data-dir (my-catfile pdir ".emacs-project"))
-      (project-el-file (my-catfile pdir ".emacs-project.el")))
+      (project-el-file (my-catfile pdir ".emacs-project.el"))
+      (project-el-file2 (my-catfile pdir "emacs-project.el")))
     (message (concat "Opening emacs-project: " emacs-project-root-directory))
     ;; use editorconfig
     (editorconfig-mode 1)
@@ -19,5 +20,6 @@
     (desktop-read data-dir)
     ;; load .emacs-project.el
     (if (file-readable-p project-el-file) (load project-el-file))
+    (if (file-readable-p project-el-file2) (load project-el-file2))
     (add-hook 'kill-emacs-hook
       `(lambda () (ignore-errors (desktop-save ,data-dir t))))))

acda1b7d61c79ca800b6770c43bf89630ea58fe1
Miao, ZhiCheng <miao@superfluid.finance>
2023-01-03 19:28:54 +0200

    fine tuning of cc mode

--- my.lang-modes/cc.el
+++ my.lang-modes/cc.el
@@ -1,25 +1,26 @@
 (require 'cc-mode)
 (require 'my-lib)
 
-(setq c-default-style "stroustrup")
-(setq-default c-basic-offset 4)
-
-(dolist (m '(c-mode-hook c++-mode-hook))
-  (add-hook m (lambda ()
-                (c-set-offset 'arglist-intro '+)
-                (c-set-offset 'innamespace 0)
-                )))
-
-;; auto hungry mode
-(c-toggle-auto-hungry-state t)
+(defun my-cc-mode-hook ()
+  (setq c-basic-offset 4)
+  (setq c-default-style "stroustrup")
+  ;; auto hungry mode
+  ;; (c-toggle-auto-hungry-state t)
+  (c-set-offset 'arglist-intro '+)
+  (c-set-offset 'innamespace 0))
 
 ;; associate hpp files with c++-mode
 (setq auto-mode-alist
   (append '(("\\.hpp$" . c++-mode))
     auto-mode-alist))
 
-(push (make-my-lang-mode :to-hook 'c-mode-hook) my-prog-lang-modes)
+(push (make-my-lang-mode
+        :to-hook 'c-mode-hook
+        :my-hook #'my-cc-mode-hook
+        :org-babel-lang 'C)
+  my-prog-lang-modes)
 (push (make-my-lang-mode
         :to-hook 'c++-mode-hook
+        :my-hook #'my-cc-mode-hook
         :org-babel-lang 'C)
   my-prog-lang-modes)

5e0a9e6d372908dce6c1f6201628e413d41cbd89
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-07-06 23:30:47 +0300

    add yul support

--- my.lang-modes/solidity.el
+++ my.lang-modes/solidity.el
@@ -2,5 +2,7 @@
 (require 'my-lib)
 
 (use-package solidity-mode)
-
 (push (make-my-lang-mode :to-hook 'solidity-mode-hook) my-prog-lang-modes)
+
+(use-package yul-mode)
+(push (make-my-lang-mode :to-hook 'yul-mode-hook) my-prog-lang-modes)

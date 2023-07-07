533d23237507e3ba5714815be5560a4c98cc31c0
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-07-07 19:28:08 +0300

    added solidity-load-buffer keybinding

--- my.lang-modes/solidity.el
+++ my.lang-modes/solidity.el
@@ -2,8 +2,18 @@
 (require 'my-lib)
 (require 'evil-ex)
 
-(use-package solidity-mode)
+;; solidity-mode
+(defun solidity-load-buffer ()
+  (interactive)
+  (save-buffer (current-buffer))
+  (let ((evil-ex-current-buffer (current-buffer))) (evil-ex-execute "!solc --via-ir --ir %")))
+
+(use-package solidity-mode
+  :bind (:map solidity-mode-map ("C-c C-l" . #'solidity-load-buffer)))
+
 (push (make-my-lang-mode :to-hook 'solidity-mode-hook) my-prog-lang-modes)
+
+;; yul-mode
 
 (defun yul-load-buffer ()
   (interactive)
@@ -11,6 +21,5 @@
   (let ((evil-ex-current-buffer (current-buffer))) (evil-ex-execute "!solc --strict-assembly %")))
 
 (use-package yul-mode
-  :bind (:map yul-mode-map
-              ("C-c C-l" . #'yul-load-buffer)))
+  :bind (:map yul-mode-map ("C-c C-l" . #'yul-load-buffer)))
 (push (make-my-lang-mode :to-hook 'yul-mode-hook) my-prog-lang-modes)

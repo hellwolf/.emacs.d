821b1c801bafe1995c95ef73b1f32b80607fad94
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-07-07 19:10:48 +0300

    added yul-mode & yul-load-buffer key binding

--- my.lang-modes/solidity.el
+++ my.lang-modes/solidity.el
@@ -1,8 +1,16 @@
 (require 'use-package)
 (require 'my-lib)
+(require 'evil-ex)
 
 (use-package solidity-mode)
 (push (make-my-lang-mode :to-hook 'solidity-mode-hook) my-prog-lang-modes)
 
-(use-package yul-mode)
+(defun yul-load-buffer ()
+  (interactive)
+  (save-buffer (current-buffer))
+  (let ((evil-ex-current-buffer (current-buffer))) (evil-ex-execute "!solc --strict-assembly %")))
+
+(use-package yul-mode
+  :bind (:map yul-mode-map
+              ("C-c C-l" . #'yul-load-buffer)))
 (push (make-my-lang-mode :to-hook 'yul-mode-hook) my-prog-lang-modes)

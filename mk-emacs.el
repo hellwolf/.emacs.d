c03e1984ee900c359c0a7f539a102d37063ddb73
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-08-30 13:58:05 +0300

    commit mk-emacs.el

---
+++ mk-emacs.el
@@ -0,0 +1,51 @@
+# AUTO-GENERATED! DO NOT DELETE!
+# This is the emacs nix package builder based on the use-packages needed.
+pkgs: emacsVariant: pkgs.buildEnv {
+  name = "hw-emacs";
+  paths = [((pkgs.emacsPackagesFor emacsVariant).emacsWithPackages (epkgs: with epkgs; [
+    all-the-icons
+bm
+browse-kill-ring
+cmake-mode
+company
+diminish
+editorconfig
+elisp-lint
+evil
+evil-org
+evil-terminal-cursor-changer
+flycheck
+fzf
+gnuplot
+haskell-mode
+highlight-symbol
+htmlize
+js2-mode
+ligature
+lsp-grammarly
+lsp-haskell
+lsp-mode
+magit
+nix-mode
+ob-typescript
+org
+org-bullets
+ox-reveal
+plantuml-mode
+solidity-mode
+tabbar
+treemacs
+treemacs-evil
+typescript-mode
+undo-tree
+vterm
+vterm-toggle
+wgrep
+which-key
+which-key-posframe
+yaml-mode
+yul-mode
+zenburn-theme
+    ]))
+  ];
+}

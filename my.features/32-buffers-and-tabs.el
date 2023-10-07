c5a56dad47668509727cc141134bccc536bb1c8e
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-10-08 00:38:24 +0300

    update my-tabbar-buffer-groups magit support

--- my.features/32-buffers-and-tabs.el
+++ my.features/32-buffers-and-tabs.el
@@ -1,17 +1,17 @@
 (require 'use-package)
 (require 'tabbar nil t)
 
+(defun my-tabbar-buffer-groups ()
+  (list (cond
+         ((string-prefix-p "*term " (buffer-name))                "terms")
+         ((string-prefix-p "*scratch" (buffer-name))              "scratches")
+         ((string-match    "[@*]irc\..*[*]?$" (buffer-name))      "irc")
+         ((string-match    "^magit.*: " (buffer-name))            "magit")
+         ((string-prefix-p "TAGS<" (buffer-name))                 "tags")
+         ((string-equal    "*" (substring (buffer-name) 0 1))     "info")
+         (t                                                       "user"))))
 (use-package tabbar
   :config
-  (defun my-tabbar-buffer-groups ()
-    (list (cond
-            ((string-prefix-p "*term " (buffer-name))         "terms")
-            ((string-prefix-p "*scratch" (buffer-name))       "scratches")
-            ((string-match "[@*]irc\..*[*]?$" (buffer-name))  "irc")
-            ((string-prefix-p "magit: " (buffer-name))        "magit")
-            ((string-prefix-p "TAGS<" (buffer-name))          "tags")
-            ((string-equal "*" (substring (buffer-name) 0 1)) "info")
-            (t                                                "user"))))
   (setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
   :init
   (tabbar-mode))
@@ -27,8 +27,8 @@
 (global-set-key (kbd "C-<tab>") 'tabbar-forward-tab)
 (global-set-key (kbd "C-<iso-lefttab>") 'tabbar-backward-tab)
 (global-set-key (kbd "C-x C-<tab>")
-  (lambda ()
-    "Switch to previously open buffer.
+                (lambda ()
+                  "Switch to previously open buffer.
 Repeated invocations toggle between the two most recently open buffers."
-    (interactive)
-    (switch-to-buffer (other-buffer (current-buffer) 1))))
+                  (interactive)
+                  (switch-to-buffer (other-buffer (current-buffer) 1))))

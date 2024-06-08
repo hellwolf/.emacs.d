7ea1c72d2bfddef964bc6ca5be6188aad17037fc
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2024-06-08 16:07:12 +0300

    nix-mode: nix-format-before-save

--- my.lang-modes/my-nix.el
+++ my.lang-modes/my-nix.el
@@ -1,6 +1,10 @@
+(require 'nix-mode nil t)
 (require 'my-lib)
 
-(use-package nix-mode)
+(use-package nix-mode
+  :custom
+  (fill-column 100)
+  :hook ((nix-mode . (lambda () (add-hook 'before-save-hook #'nix-format-before-save)))))
 
 (push (make-my-lang-mode
        :to-hook 'nix-mode-hook)

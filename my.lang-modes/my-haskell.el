32f2118f81683fc6a42cc699ff057a66575eb24b
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2024-09-17 16:32:56 +0300

    fix haskell mode

--- my.lang-modes/my-haskell.el
+++ my.lang-modes/my-haskell.el
@@ -4,27 +4,7 @@
 (require 'flycheck nil t)
 (require 'my-lib)
 
-(use-package haskell-mode
-  :pin manual
-  :custom
-  (haskell-stylish-on-save t)
-  (haskell-font-lock-symbols t)
-  ;; https://github.com/haskell/haskell-language-server/discussions/3105
-  (lsp-lens-place-position 'above-line)
-  ;; (haskell-font-lock-symbols t)
-  :hook ((haskell-mode . #'haskell-mode-init)
-         (haskell-literate-mode . #'haskell-mode-init))
-  :bind (:map
-         haskell-mode-map
-         ;; https://emacs.stackexchange.com/questions/59254/how-to-bind-key-in-use-package/59269#59269
-         ("C-x C-r" . #'my-haskell-load-and-run)
-         :map
-         interactive-haskell-mode-map
-         ("M-." . #'xref-find-definitions)))
-
-(use-package lsp-haskell)
-
-(defun haskell-mode-init()
+(defun my-haskell-mode-init()
   (interactive-haskell-mode)
   (haskell-doc-mode)
   (lsp)
@@ -43,15 +23,34 @@
     (pop-to-buffer start-buffer)
     (my-jump-to-register)))
 
+(use-package haskell-mode
+  :pin manual
+  :custom
+  (haskell-stylish-on-save t)
+  (haskell-font-lock-symbols t)
+  ;; https://github.com/haskell/haskell-language-server/discussions/3105
+  (lsp-lens-place-position 'above-line)
+  ;; (haskell-font-lock-symbols t)
+  :hook ((haskell-mode . my-haskell-mode-init)
+         (haskell-literate-mode . my-haskell-mode-init))
+  :bind (:map
+         haskell-mode-map
+         ;; https://emacs.stackexchange.com/questions/59254/how-to-bind-key-in-use-package/59269#59269
+         ("C-x C-r" . #'my-haskell-load-and-run)
+         :map
+         interactive-haskell-mode-map
+         ("M-." . #'xref-find-definitions))
+  :init
+  ;; Disabling stack-ghc flychecker when I am using stack, it is very slow otherwise.
+  ;; See: https://emacs.stackexchange.com/questions/53667/how-to-disable-stack-with-flycheck-for-haskell
+  (add-to-list 'flycheck-disabled-checkers 'haskell-stack-ghc)
+  ;; Display lambda symbol instead of backslash.
+  (custom-set-variables
+   '(haskell-font-lock-symbols-alist '(("\\" . "λ")))))
+
+(use-package lsp-haskell)
+
 (push (make-my-lang-mode
        :to-hook        'haskell-mode-hook
        :org-babel-lang 'haskell)
       my-prog-lang-modes)
-
-;; Disabling stack-ghc flychecker when I am using stack, it is very slow otherwise.
-;; See: https://emacs.stackexchange.com/questions/53667/how-to-disable-stack-with-flycheck-for-haskell
-(add-to-list 'flycheck-disabled-checkers 'haskell-stack-ghc)
-
-;; Display lambda symbol instead of backslash.
-(custom-set-variables
- '(haskell-font-lock-symbols-alist '(("\\" . "λ"))))

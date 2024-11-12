b45a28d80dad79bdd23698a94eaa90432f5568a0
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2024-11-12 11:25:49 +0200

    rcirc-omit-responses 1

--- my.features/77-rcirc.el
+++ my.features/77-rcirc.el
@@ -4,4 +4,6 @@
 (add-hook 'rcirc-mode-hook
   (lambda ()
     (flyspell-mode 1)
+    (rcirc-omit-responses 1)
+    (setopt rcirc-omit-responses '("JOIN" "PART" "QUIT" "NICK" "AWAY"))
     (rcirc-track-minor-mode 1)))

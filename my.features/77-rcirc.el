ea183f1973dff88f791e31c2ab344d297e1206f0
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2024-12-28 23:45:33 +0200

    rcirc-fill-column -> 100

--- my.features/77-rcirc.el
+++ my.features/77-rcirc.el
@@ -5,7 +5,7 @@
   :custom
   (rcirc-reconnect-delay 60)
   (rcirc-time-format "%m-%d %H:%M ")
-  (rcirc-fill-column 80)
+  (rcirc-fill-column 100)
   (rcirc-omit-responses '("JOIN" "PART" "QUIT" "NICK" "AWAY"))
   (rcirc-omit-unless-requested '("NAMES"))
   :hook (rcirc-mode . my-rcirc-mode-hook)

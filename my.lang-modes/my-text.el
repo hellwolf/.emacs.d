620822c0d72b07d4b6e545e0931f94c9cedd37f6
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2025-02-13 20:32:41 +0200

    add flycheck-grammarly to text mode

--- my.lang-modes/my-text.el
+++ my.lang-modes/my-text.el
@@ -1,5 +1,9 @@
 (require 'my-lib)
 
+(use-package flycheck-grammarly
+  :pin manual
+  :init (flycheck-grammarly-setup))
+
 (push (make-my-lang-mode
        :to-hook 'text-mode-hook)
       my-text-lang-modes)
